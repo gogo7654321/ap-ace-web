c79713556bfc08d5ef5f645305ad0d9626682a1b
gogo7654321 <npatel012010@gmail.com>
2025-04-11 19:49:48 -0400

    Updated image paths in col-13

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/ap-ace-web' : '',
-  assetPrefix: isProd ? '/ap-ace-web' : '',
+  assetPrefix: isProd ? '/ap-ace-web/' : '',
+  images: {
+    unoptimized: true, // <-- Important for static exports on GitHub Pages
+  },
 };
 
 export default nextConfig;
